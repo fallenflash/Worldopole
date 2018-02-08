049c683c8c305d476f8aa1e78f398655b8bc5cd2
fallenflash <littlemantis90@gmail.com>
2018-02-08 05:59:50 -0500

    Auto stash before merge of "merge" and "master"

---
+++ pages/news.page.php
@@ -0,0 +1,101 @@
+<div class="row-title" style="color:#e1dced;">
+    <h3>Hey Everyone, Welcome</h3>
+</div>
+<div class="body" style="color:#e1dced;">
+
+<div class="row">
+    <div class="col-sm-8">
+<p>
+    We're moving up in the world, arn't we, got our own .com
+</p>
+
+<p>
+    The new home of our site is going to be WHGPoGo.com, this includes the map, the stats site, and the soon to come full website and wiki!&nbsp; And who remembers when I made that survey back around Christmas? Well don't worry we listened. The biggest things people talked about were; Direct Message notifications, IV scanning, Coverage Area, and map Load Times. Today I bring you probably one of the largest announcements we've had since we started mapping. So please stay with me, as there are 2 big things, a couple medium, and a couple small.
+</p>
+</div>
+<div id="logonewspic" class="col-sm-4" style="position: relative; text-align: center; color: white;">
+     <img src="https://teamrocketmap.ddns.net/images/5.png" alt="Norway" style="width:100%; max-width: 20vw;">
+    <div class="centered" style="position: absolute;color: #FFF; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000; top: 40%; left: 50%; transform: translate(-50%, -50%);">
+        <a href=whgpogo.com style="text-decoration: none !important; font-size: 100%"><<h1><strong>WHGPoGo.com!!!</strong></h1></a>
+    </div>
+</div>
+</div>
+
+<p></p>
+
+<div>
+    <strong>First:</strong> Let's start with the one that's been talked about in the chat rooms. IV scanning. As I'm sure you've noticed by now as we've been testing this for the last few days. It's stable, awesome, and as long as nothing unforeseen happens, it's here to stay. I would like to give a shout-out to the donators that funded this venture; @castlekins, @CurlyWFan, @jvanwert3, and @fairladyz. As well as a shoutout to @enzikami and @fairladyz of Manassas for all the collaborative efforts and continued help in sustainability.
+</div>
+
+<div>
+    <sub>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; *Please note for those of you that are not Level 30: These IVs, Stats, and even Move-sets will not be accurate, but you'll see the awesome stuff we post, and just let that be your motivation for leveling up.
+    </sub>
+</div>
+
+<p>&nbsp;</p>
+
+<div>
+    <strong>Second:</strong>
+
+    Our new bot, The Collector is now in action. It will not only replace our old bots, but drum roll please Provide Direct Message notifications that you are able to personalize. The bot will take commands from #bot-commands as well as through the private messaging system Discord has in place.. So swing on over, get notified, and if you have any questions, come to me or any of the staff. I've also made a guide to help everyone because it can be a little complicated. But it's totally worth the payoff.
+</div>
+
+<div>&nbsp;</div>
+
+<div>
+    Check out the guide at
+
+    <a href="https://teamrocketmap.ddns.net/warrentonstats/thecollector">TheCollector</a>
+</div>
+
+<div>
+    And Check out the region map at
+
+    <a href="https://teamrocketmap.ddns.net/warrentonstats/whgmap">WHG Area Map</a>
+</div>
+
+<div>&nbsp;</div>
+
+<div>Aaaannd more!!!</div>
+
+<div>&nbsp;</div>
+
+<div>
+    <strong>Third:&nbsp;</strong>
+
+    The first part of this is only for those on iOS devices there is&nbsp;
+
+    <a href="https://goo.gl/rw7LgA">Thunder Radar</a>
+
+    . ThunderRadar has no association with us, but I have enabled our maps to work on it just go to settings and put our map into it, load images, and your good to go. Again our staff will help you through this.
+</div>
+
+<div>
+    For the second part of this: and this applies to everyone, take a look at the new webpage on our map. This is in addition to our normal one, and right now is at
+
+    <a href="https://teamrocketmap.ddns.net/mapb">Map Beta</a>
+
+    . This is a work in progress, and not quite as polished as the normal one, but it already boasts more features having to do with IVs, as well as a much faster load time. This is the next step of projects, so definitely keep an eye on it.
+</div>
+
+<p>&nbsp;</p>
+
+<div>
+    <strong>Fourth &amp; Last:</strong>
+
+    You guys wanted more coverage, well you've got it. Included Remington and Nokesville into the mix, along with tons of gyms in the surrounding areas, Midland, Jeffersontown, Airlie, Catlett. So in addition, to /Warrenton our map also works with the following: /Bealeton, /Remington, /Haymarket, /Gainesville, /Bristow, /Nokesville, /Vinthill, and will even take you to neighboring sites with /Culpeper and /Manassas.
+</div>
+
+<p>&nbsp;</p>
+
+<div>
+    So again, I know this was a lot to take in, but we do listen. We also want to thank you for making this group what it is today.
+</div>
+
+<p>&nbsp;</p>
+
+<div>
+    So be safe, stay awesome, upgrade, and Catch 'Em All!
+</div>
+</div>
