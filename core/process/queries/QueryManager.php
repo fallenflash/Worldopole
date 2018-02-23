45f642845eb3416c5ed89d9711e28be7d31c759e
Florian Kostenzer <florian.kostenzer@yahoo.de>
2018-02-23 15:39:24 +0100

    split nest calc into 0.5x0.5 areas

--- core/process/queries/QueryManager.php
+++ core/process/queries/QueryManager.php
@@ -96,5 +96,6 @@ abstract class QueryManager {
 	abstract public function getPokemonSinceLastUpdate($pokemon_id, $last_update);
 	abstract public function getRaidsSinceLastUpdate($pokemon_id, $last_update);
 	abstract public function getCaptchaCount();
-	abstract public function getNestData($time);
+	abstract public function getNestData($time, $minLatitude, $maxLatitude, $minLongitude, $maxLongitude);
+	abstract public function getSpawnpointCount($minLatitude, $maxLatitude, $minLongitude, $maxLongitude);
 }
